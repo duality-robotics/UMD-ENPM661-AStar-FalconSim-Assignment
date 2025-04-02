7159731025ecc399582aebc5b3336c9f0d3f660b
Rishikesh Jadhav <94460336+Rishikesh-Jadhav@users.noreply.github.com>
2025-04-02 12:03:43 -0500

    Add files via upload

---
+++ ROS2/falcon_turtlebot3_project_ws/src/astar_falcon_planner/astar_falcon_planner/submodules/astar_planner.py
@@ -0,0 +1,17 @@
+
+def plan_path(start,
+              end,
+              robot_radius,
+              clearance,
+              delta_time,
+              goal_threshold,
+              wheel_radius,
+              wheel_distance,
+              rpm1,
+              rpm2):
+
+    ### Fill in your A* planner here!!!
+    # Output path should be in the format List[List[dx,dy,dtheta]] with units cm and radians [[8, 0, 0.7854],[14,14,0]...]
+
+    return path
+
